062ccc4a61d3e4f6c4c3101e690bdda964a5f269
892280082 <892280082@qq.com>
2015-11-11 17:23:47 +0800

    page 38

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,25 @@
 var crypto = require('crypto');
 User = require('../models/user.js');
 
+function checkIsLogin(req,res,next){
+	if(!req.session.user){
+		req.flash('error','not login');
+		res.redirect('/login');
+	}
+	next();
+}
+
+function checkIsNotLogin(req,res,next){
+	if(req.session.user){
+		req.flash('error','already login!');
+		res.redirect('back');
+	}
+	next();
+}
+
 
 module.exports = function(app){
+
 	app.get('/',function(req,res){
 		res.render('index',{
 			title:'主页',
@@ -12,6 +29,7 @@ module.exports = function(app){
 		});
 	});
 
+	app.get('/reg',checkIsNotLogin);
 	app.get('/reg',function(req,res){
 		res.render('reg',{
 			title:'注册',
@@ -21,6 +39,7 @@ module.exports = function(app){
 		});
 	});
 
+	app.post('/reg',checkIsNotLogin);
 	app.post('/reg',function(req,res){
 		var name = req.body.name,
 			password = req.body.password,
@@ -62,25 +81,58 @@ module.exports = function(app){
 			})
 	});
 
+	app.get('/login',checkIsNotLogin);
 	app.get('/login',function(req,res){
 		res.render('login',{
 			title:'主页',
+			user:req.session.user,
+			success:req.flash('error').toString(),
+			error:req.flash('success').toString()
+		});
+	});
+	
+	app.post('/login',checkIsNotLogin);
+	app.post('/login',function(req,res){
+		var md5 = crypto.createHash('md5'),
+		       password = md5.update(req.body.password).digest('hex');
+		       //check user
+		       User.get(req.body.name,function(err,user){
+		       	if(err){
+		       		req.flash('error','system error!');
+		       		return res.redirect('/login');
+		       	}
+		       	if(!user){
+		       		req.flash('error','user not exists!');
+		       		return res.redirect('/login');
+		       	}
+		       	if(user.password != password){
+		       		req.flash('error','password is not right!');
+		       		return res.redirect('/login');
+		       	}
+		       	req.session.user = user;
+		       	req.flash('success','login success!');
+		       	res.redirect('/');
+		       })
+	});
+
+	app.get('/post',checkIsLogin);
+	app.get('/post',function(req,res){
+		res.render('index',{
+			title:'发布',
 			user:req.session.user,
 			success:req.flash('success').toString(),
 			error:req.flash('error').toString()
 		});
 	});
-	
-	app.post('/login',function(req,res){
-	});
 
-	app.get('/post',function(req,res){
-		res.render('index',{title:'发布'});
-	});
-
+	app.post('/post',checkIsLogin);
 	app.post('/post',function(req,res){
 	});
 
+	app.get('/logout',checkIsLogin);
 	app.get('/logout',function(req,res){
+		req.session.user = null;
+		req.flash('success','logout success!');
+		res.redirect('/');
 	});
 }
