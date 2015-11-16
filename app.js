460b278a16f743c713a38e3f02b3234bd50a3605
892280082 <892280082@qq.com>
2015-11-16 17:06:23 +0800

    page 85 ok

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var flash = require('connect-flash');
 var multer = require('multer');
+var fs = require('fs');
+var accessLog = fs.createWriteStream('access.log',{flags:'a'});
+var errorLOg = fs.createWriteStream('error.log',{flags:'a'});
 var app = express();
 
 
@@ -22,10 +25,18 @@ app.set("port",3000);
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(flash());
 app.use(logger('dev'));
+app.use(logger({stream:accessLog}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(err,req,res,next){
+  var meta = '[' + new Date() +']' +req.url +'\n';
+  errorLog.write(meta+err.stack+'\n');
+  next();
+});
+
 app.use(multer({
   dest:'./public/images',
   rename:function(fieldname,filename){
